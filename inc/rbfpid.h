154c28a96d1bfeaeffd8b32e771cec0d5962a09a
Dictor <kimdictor@gmail.com>
2023-08-17 11:31:57 +0900

    RBFPID constructor WIP

--- inc/rbfpid.h
+++ inc/rbfpid.h
@@ -7,10 +7,29 @@ namespace RbfpidBalbot {
 namespace control {
 class RBFPID {
  private:
+  /* PID controller parameters */
+  double ek_, ek1_, ek2_;  // e(k), e(k-1), e(k-2)
+  double sumek_;
+  double xc1_, xc2_, xc3_;  // xc(1), xc(2), xc(3)
+  double u_, du_;
+  double kp_, ki_, kd_;
+
+  /* RBF NN parameters */
+  int n_, m_;
+  Eigen::MatrixXd X_;               // input X [n x 1]
+  Eigen::MatrixXd Ck_, Ck1_, Ck2_;  // center vector C [m x n]
+  Eigen::MatrixXd H_;               // radial vector [m x 1]
+  Eigen::MatrixXd Bk_, Bk1_, Bk2_;  // radial width vector B [m x 1]
+  Eigen::MatrixXd Wk_, Wk1_, Wk2_;  // weight vector W [m x 1]
+  double yk_, yk1_;                 // output of plant
+  double ym;                        // output of RBF NN
+  double J;                         // jacobian information of RBF NN
  public:
   /**
    * @brief Construct a new RBFPID object
    *
+   * @param n size of input
+   * @param m size of hidden
    * @param mg momentum factor
    * @param gain_mg momentum factor for only PID gain changing
    * @param lr learning rate
@@ -21,12 +40,12 @@ class RBFPID {
    * @param init_rbf_width initial rbf nn width
    * @param init_rbf_weight initial rbf nn weight
    */
-  RBFPID(double gain_mg, double init_kp, double init_ki,
-         double init_kd, double u_limit, double init_rbf_width = 3.0,
+  RBFPID(int n, int m, double gain_mg, double init_kp, double init_ki, double init_kd,
+         double u_limit, double init_rbf_width = 3.0,
          double init_rbf_weight = 0.01, double mg = 0.25, double lr = 0.05);
   /**
    * @brief Update controller
-   * 
+   *
    * @param e error (input of controller)
    * @param y result (output of plant)
    * @return double output (input of plant)
--- src/rbfpid.cpp
+++ src/rbfpid.cpp
@@ -3,3 +3,38 @@
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(control);
+
+using namespace RbfpidBalbot::control;
+using namespace Eigen;
+
+RBFPID::RBFPID(int n, int m, double gain_mg, double init_kp, double init_ki,
+               double init_kd, double u_limit, double init_rbf_width,
+               double init_rbf_weight, double mg, double l)
+    : ek1_(0.0),
+      ek2_(0.0),
+      sumek_(0.0),
+      u_(0.0),
+      du_(0.0),
+      kp_(init_kp),
+      ki_(init_ki),
+      kd_(init_kd),
+      X_(n, 1),
+      Ck_(m, n),
+      Ck1_(m, n),
+      Ck2_(m, n),
+      H_(m, 1),
+      Bk_(m, 1),
+      Bk1_(m, 1),
+      Bk2_(m, 1),
+      Wk_(m, 1),
+      Wk1_(m, 1),
+      Wk2_(m, 1),
+      yk1_(0) {
+        X_ = MatrixXd::Zero(m, 1);
+        Ck_ = MatrixXd::Zero(m, n);
+        Ck1_ = MatrixXd::Zero(m, n);
+        Ck2_ = MatrixXd::Zero(m, n);
+        H_ = MatrixXd::Zero(m, 1);
+      }
+
+double RBFPID::Update(double e, double y) {}
