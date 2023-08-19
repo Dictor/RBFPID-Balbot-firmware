2b0bdf4e837441378cf9f298af0a00b446a288dc
Dictor <kimdictor@gmail.com>
2023-08-19 20:00:38 +0900

    Implement RBF-PID logic

--- inc/rbfpid.h
+++ inc/rbfpid.h
@@ -11,19 +11,21 @@ class RBFPID {
   double ek_, ek1_, ek2_;  // e(k), e(k-1), e(k-2)
   double sumek_;
   double xc1_, xc2_, xc3_;  // xc(1), xc(2), xc(3)
-  double u_, du_;
+  double u_, du_, u_limit_;
   double kp_, ki_, kd_;
+  
 
   /* RBF NN parameters */
-  int n_, m_;
+  const int n_, m_;
   Eigen::MatrixXd X_;               // input X [n x 1]
   Eigen::MatrixXd Ck_, Ck1_, Ck2_;  // center vector C [m x n]
   Eigen::MatrixXd H_;               // radial vector [m x 1]
   Eigen::MatrixXd Bk_, Bk1_, Bk2_;  // radial width vector B [m x 1]
   Eigen::MatrixXd Wk_, Wk1_, Wk2_;  // weight vector W [m x 1]
   double yk_, yk1_;                 // output of plant
-  double ym;                        // output of RBF NN
-  double J;                         // jacobian information of RBF NN
+  double ym_;                       // output of RBF NN
+  double J_;                        // jacobian information of RBF NN
+  double lr_, mg_, gain_mg_;        // learning rate and momentum factor
  public:
   /**
    * @brief Construct a new RBFPID object
@@ -40,8 +42,8 @@ class RBFPID {
    * @param init_rbf_width initial rbf nn width
    * @param init_rbf_weight initial rbf nn weight
    */
-  RBFPID(int n, int m, double gain_mg, double init_kp, double init_ki, double init_kd,
-         double u_limit, double init_rbf_width = 3.0,
+  RBFPID(int n, int m, double gain_mg, double init_kp, double init_ki,
+         double init_kd, double u_limit, double init_rbf_width = 3.0,
          double init_rbf_weight = 0.01, double mg = 0.25, double lr = 0.05);
   /**
    * @brief Update controller
